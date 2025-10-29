2c85d69e6799299c75a318a8c217dfc600fc5278
alexfavero4036@gmail.com <alexfavero4036@gmail.com>
2025-10-29 14:13:00 -0300

    Melhorando implementacao do metodo imprimir tabuleiro

--- Program.cs
+++ Program.cs
@@ -10,8 +10,8 @@ internal class Program
             Tabuleiro tab = new Tabuleiro(8, 8);
 
             tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 9));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 7));
+            tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
 
             Tela.imprimirTabuleiro(tab);
         }
